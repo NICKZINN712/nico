8c5436735eb063ea47461a2278daa7bd54005373
NICKZINN712 <140556419+NICKZINN712@users.noreply.github.com>
2024-10-04 11:15:55 -0300

    .

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-function soma(num1,num2) {
-    return num1+num2
-}
-console.log(soma(6,4))
 
-function areaQuadrado(lado) {
-    return lado 
+
+function areQuadrado(lado) {
+    return lado**2
+}
+console.log (areQuadrado(2,2) );
+
+let media=function (num1,num2,num3,){
+    return (num1+num2+num3)/num3;
 
 }
- console.log(areaQuadrado(6))
- 
+console.log(media(1,2,3));
+
+
+let nome= parseInt(prompt(1+2));
+document.write
