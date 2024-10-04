75369f176ae0feea55c257d7f145df0eb1eae439
NICKZINN712 <140556419+NICKZINN712@users.noreply.github.com>
2024-10-04 11:50:20 -0300

    k

--- main.js
+++ main.js
@@ -16,3 +16,11 @@
 // document.write()
 
 let estados=["ac","al","ap","am","ba","ce","df","es","go","ma","mt","ms","mg","pa","pb","pr","pe","pi","rj","rn","rs","ro","rr","sc","sp","se","to"]
+
+let i=0;
+while (i<10) {
+    console.log(i);
+  console.log(estados[1]);
+    i++;
+
+}
